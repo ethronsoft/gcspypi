7b669fc15f50d5d84d5ddc09078fd92ab9d8540a
edd <ef@ethronsoft.com>
2017-03-21 13:51:26 +0000

    added some install impl

--- gcspypi/pb.py
+++ gcspypi/pb.py
@@ -3,102 +3,129 @@ import shutil
 import os
 import glob
 import json
-import yaml
+import tempfile
+
 
 class Package(object):
-	def __init__(self, name, version):
-		self.__name = name
-		self.__version = self.__checked_version(version)
-		
-	def __checked_version(self,v):
-		if (len(v.split(".")) != 3): raise Exception("Version must be provided in major.minor.patch format")
-		return v
-		
-	def __del__(self):
-		print "Package destructor"
-		
-	def prepare(self):
-		return "some"
-		
-	@property
-	def name(self):
-		return self.__name
-				
-	@property
-	def version(self):
-		return self.__name
+    def __init__(self, name, version, requirements):
+        self.__name = name
+        self.__version = self.__checked_version(version) if version else ""
+        self.__requirements = requirements
+
+    def __checked_version(self, v):
+        if (len(v.split(".")) != 3): raise Exception("Version must be provided in major.minor.patch format")
+        return v
+
+    def __del__(self):
+        print "Package destructor"
+
+    def prepare(self):
+        return "some"
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def full_name(self):
+        return self.name + ":" + self.version if (self.version != "") else self.name
+
+    @property
+    def requirements(self):
+        return self.__requirements
+
+    @property
+    def version(self):
+        return self.__name
+
 
 class PackageParser(object):
-	def __init__(self, syntax):
-		pass
-	
-		
-	def parse(self):
-		pass
-		
-class PackageBuilder(object):
+    def __init__(self, syntax):
+        pass
 
-	def __init__(self,raw_package):
-		if 	(".zip" in raw_package or ".tar" in raw_package):
-			self.__info = __extract_source(raw_package)
-		elif(".egg" in raw_package):
-			self.__info = __extract_egg(raw_package)
-		elif(".whl" in raw_package):
-			self.__info = __extract_wheel(raw_package)
-		else:
-			raise Exception("Unrecognized file extension. expected {.zip|.tar|.tar.gz|.egg|.whl}")	
-			
-	def __seek_and_apply(self, raw_package, target, command):
-		try:
-			cwd = os.getcwd()
-			dir = tempfile.mkdtemp()
-			os.chdir(dir)
-			with zipfile.ZipFile(raw_package,"r") as f:
-				f.extractall(".")
-				with root,dir,file in os.walk("."):
-					if file == target:
-						with open(os.path.join(root,file),"r") as target_file:
-							command(target_file)
-						break
-		finally:
-			shutil.rmtree(dir)
-			os.chdir(cwd)
-			
-	def __extract_egg(self,raw_package):
-		#egg needs to get data from PKG-INFO, just like source...
-		return self.__extract_source(raw_package)		
-		
-	def __extract_source(self,raw_package):
-		class Apply(object):
-			def __init__(self):
-				self.metadata = {}
-			def __call__(self, target):	
-				map = {}
-				for line in target.readlines():
-					k,v = line.split(":")
-					map[k] = v
-				metadata["name"] = map["name"]
-				metadata["version"] = map["version"]
-		apply = Apply()			
-		self.__seek_and_apply(raw_package,"PKG-INFO",apply)
-		if not apply.metadata:
-			raise Exception("Could not find PKG-INFO in: " + raw_package)
-		return 	apply.metadata
-			
-	def __extract_wheel(self,raw_package):
-		class Apply(object):
-			def __init__(self):
-				self.metadata = {}
-			def __call__(self, target):	
-				map = json.loads(target,"r").read())
-				metadata["name"]= map["name"]
-				metadata["version"] = map["version"]
-					
-		apply = Apply()			
-		self.__seek_and_apply(raw_package,"metadata.json",apply)
-		if not apply.metadata:
-			raise Exception("Could not find PKG-INFO in: " + raw_package)
-		return 	apply.metadata
-	
-	def build(self):
-		return Package(self.__info.name, self.__info.version)
+    def parse(self):
+        pass
+
+
+class PackageBuilder(object):
+    def __init__(self, raw_package):
+        if ".zip" in raw_package or ".tar" in raw_package:
+            self.__info = self.__extract_source(raw_package)
+        elif ".egg" in raw_package:
+            self.__info = self.__extract_egg(raw_package)
+        elif ".whl" in raw_package:
+            self.__info = self.__extract_wheel(raw_package)
+        else:
+            raise Exception("Unrecognized file extension. expected {.zip|.tar|.tar.gz|.egg|.whl}")
+
+    def __seek_and_apply(self, raw_package, target, command):
+        try:
+            cwd = os.getcwd()
+            dir = tempfile.mkdtemp()
+            os.chdir(dir)
+            with zipfile.ZipFile(raw_package, "r") as f:
+                f.extractall(".")
+                for r, dir, f in os.walk("."):
+                    if f == target:
+                        with open(os.path.join(r, file), "r") as target_file:
+                            command(target_file)
+                        break
+        finally:
+            shutil.rmtree(dir)
+            os.chdir(cwd)
+
+    def __extract_egg(self, raw_package):
+        # egg needs to get data from PKG-INFO, just like source...
+        return self.__extract_source(raw_package)
+
+    def __extract_source(self, raw_package):
+        class InfoCmd(object):
+            def __init__(self):
+                self.metadata = {}
+
+            def __call__(self, target):
+                map = {}
+                for line in target.readlines():
+                    k, v = line.split(":")
+                    map[k] = v
+                self.metadata["name"] = map["name"]
+                self.metadata["version"] = map["version"]
+
+        cmd = InfoCmd()
+        self.__seek_and_apply(raw_package, "PKG-INFO", cmd)
+        if not cmd.metadata:
+            raise Exception("Could not find PKG-INFO in: " + raw_package)
+        requirements = self.__read_requirements(raw_package)
+        return {"name": cmd.metadata.name, "version": cmd.metadata.version, "requirements": requirements}
+
+    def __extract_wheel(self, raw_package):
+        class InfoCmd(object):
+            def __init__(self):
+                self.metadata = {}
+
+            def __call__(self, target):
+                map = json.loads(target.read())
+                self.metadata["name"] = map["name"]
+                self.metadata["version"] = map["version"]
+
+        cmd = InfoCmd()
+        self.__seek_and_apply(raw_package, "metadata.json", cmd)
+        if not cmd.metadata:
+            raise Exception("Could not find PKG-INFO in: " + raw_package)
+        requirements = self.__read_requirements(raw_package)
+        return {"name": cmd.metadata.name, "version": cmd.metadata.version, "requirements": requirements}
+
+    def __read_requirements(self, raw_package):
+        class RequiresCmd(object):
+            def __init__(self):
+                self.requires = []
+
+            def __call__(self, target):
+                self.requires = target.readlines()
+
+        cmd = RequiresCmd()
+        self.__seek_and_apply(raw_package, "requires", cmd)
+        return cmd.requires
+
+    def build(self):
+        return Package(self.__info.name, self.__info.version, set(self.__info.requirements))
--- gcspypi/pm.py
+++ gcspypi/pm.py
@@ -1,48 +1,136 @@
 from google.cloud import storage
-from pb import Package
+from pb import *
+
 
 class PackageManager(object):
-	
-	def __init__(self, repo_name):
-		self.__bucket_name = repo_name
-		pass
-		
-	def __get_bucket(self):
-		pass
-		###return a client using the current default login 
-		###set with: gcloud auth application-default login
-		#return storage.Client().bucket(self.__bucket_name)
-		
-	def upload(self, pkg, overwrite):
-		print "uploading"
-		pass
-		#repo_object_name = "some"
-		#blob = self.__get_bucket.blob(repo_object_name)
-		#blob.upload_from_filename(filename)
-		
-	def download(self, pkg):
-		print "downloading"
-		pass
-		#target_file_name = "some.in"
-		#output_file_name = "some.out"
-		#blob = self.__get_bucket.blob(repo_object_name)
-		#blob.download_to_filename(output_file_name)	
-		
-	def list(self, prefix):
-		print "listing"
-		pass
-		#list_res = self.__get_bucket.list_blobs(prefix=prefix)
-		##let's return ordered, for searching and visual reasons
-		#def comp(i, x):
-		#	if   (i.name == x.name): return 0
-		#	elif (i.name <  x.name): return -1
-		#	else				      : return 1
-		#return sorted(list_res,cmp=comp)
-	
-	def remove(self, pkg):
-		print "removing"
-		pass
-		
-	def install(self, pkg):
-		print "installing"
-		pass
+    def __init__(self, repo_name, overwrite=False, mirroring=True, install_deps=True):
+        self.__bucket_name = repo_name
+        self.__overwrite = overwrite
+        self.__mirroring = mirroring
+        self.__install_deps = install_deps
+        self.__repo_cache = []
+
+    def upload(self, pkg, overwrite):
+        print "uploading"
+
+        # repo_object_name = "some"
+        # blob = self.__get_bucket.blob(repo_object_name)
+        # blob.upload_from_filename(filename)
+
+    def download(self, pkg, dest):
+        print "downloading"
+        return False
+        # target_file_name = "some.in"
+        # output_file_name = "some.out"
+        # blob = self.__get_bucket.blob(repo_object_name)
+        # blob.download_to_filename(output_file_name)
+
+    def list(self, prefix=""):
+        print "listing"
+        # list_res = self.__get_bucket.list_blobs(prefix=prefix)
+        ## let's return ordered, for searching and visual reasons
+        # def comp(i, x):
+        #	if   (i.name == x.name): return 0
+        #	elif (i.name <  x.name): return -1
+        #	else				      : return 1
+        # return sorted(list_res,cmp=comp)
+
+    def search(self, pkgs):
+        #remove all pkgs that are not in the repo
+        return pkgs
+
+    def remove(self, pkg):
+        print "removing"
+
+    def install(self, pkg):
+        print "installing"
+        if not self.__repo_cache:
+            self.__repo_cache = list()
+
+        is_internal = False#TODO
+        if not is_internal:
+            if self.__mirroring:
+                print "mirrored pip install {}".format(pkg.full_name)
+                self.__pip_install(pkg.full_name)
+            else:
+                print "{0} not in {1} repository".format(pkg.full_nam,self.__bucket_name)
+        else:
+            try:
+                tmp = tempfile.mkdtemp()
+                root_pkg_install = os.path.join(tmp,pkg.full_name.replace(":", "_"))
+                self.download(pkg, root_pkg_install)
+
+                if self.__install_deps:
+                    #let's separate all the internal requirements from the public ones.
+                    #let's install the internal requirements ourselves and delegate the public ones to pip install.
+                    #Then, once the requirements are installed, let's call pip install on the package itself
+
+                    #packages to scan for requirements
+                    scan_targets = set([root_pkg_install])
+                    #names of internal requirements
+                    internal_reqs = set([])
+                    #names of public requirements
+                    public_reqs = set([])
+                    while scan_targets:
+                        scanned_pkg = PackageBuilder(scan_targets.pop()).build()
+                        new_internal_reqs = self.__find_internal_requirements(scanned_pkg)
+                        internal_reqs.union(new_internal_reqs)
+                        public_reqs.union(scanned_pkg.requirements - new_internal_reqs)
+                        #Let's scan the new internal requirements as they may
+                        #themselves point to more internal and public requirements.
+                        for inreq in new_internal_reqs:
+                            req_pkg = PackageParser(inreq).parse()
+                            req_pkg_install = os.path.join(tmp,req_pkg.full_name.replace(":", "_"))
+                            self.download(req_pkg,req_pkg_install)
+                            scan_targets.add(req_pkg_install)
+
+                    #let's proceed installing all public requirements first
+                    self.__public_install(public_reqs)
+                    #let's continue with our private requirements
+                    self.__internal_install(internal_reqs)
+                    #then let's use pip install to install the orignal package.
+                    #because we have already taken care of dependendencies, we can
+                    #use pip --no-dependencies flag
+                    self.__pip_install(root_pkg_install, ["--no-dependencies"])
+                else:
+                    self.__pip_install(root_pkg_install, ["--no-dependencies"])
+            finally:
+                shutil.rmtree(tmp, ignore_errors=True)
+
+
+    def uninistall(self, pkg):
+        print "uninstalling"
+
+    def clear_cache(self):
+        self.__repo_cache = []
+
+
+    def __get_bucket(self):
+        ###return a client using the current default login
+        ###set with: gcloud auth application-default login
+        # return storage.Client().bucket(self.__bucket_name)
+        pass
+
+    def __find_internal_requirements(self,pkg):
+        return set([])
+
+    def __internal_install(self, requirements, install_dir):
+        #install the private package using pip.
+        #Because all dependendencies (public or internal)
+        #that one of these packages may have is already either
+        #in this requriments list or in the one passed to
+        #__public_install,we can use pip --no-dependencies flag
+        for r in requirements:
+            #we have saved the temp packages using Package::full_name().replace(":,"_")
+            #so let's get back our package to reference that file
+            pkg = PackageParser(r).parse()
+            self.__pip_install(os.path.join(install_dir,pkg.full_name.replace(":", "_")), ["--no-dependencies"])
+
+
+    def __public_install(self, requirements):
+        for r in requirements:
+            self.__pip_install(r)
+
+    def __pip_install(self, resource, flags=[]):
+
+        pass
