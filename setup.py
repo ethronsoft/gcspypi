bb65c6628765685558309281497b6acdcf216cf8
edd <ef@ethronsoft.com>
2017-10-28 15:14:28 +0100

    including package data

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 from setuptools import setup
 
 setup(
-	name='gcspypi',
-	version='1.0.4',
-	author='Ethronsoft',
-	author_email='dev@ethronsoft.com',
-	license=open("LICENSE").read(),
-	keywords="pypi private repository gcs google cloud storage",
-	url="https://github.com/ethronsoft/gcspypi",
-	description="Private packages repository backed by Google Cloud Storage",
-	packages=['ethronsoft','ethronsoft.gcspypi'],
-	install_requires=[
-		"google-cloud-storage==1.5.0"
-	],
-	entry_points={
-		'console_scripts': [
-			'gcspypi = ethronsoft.gcspypi.__main__:main'
-		]
-	}
+    name='gcspypi',
+    version='1.0.4',
+    author='Ethronsoft',
+    author_email='dev@ethronsoft.com',
+    license=open("LICENSE").read(),
+    keywords="pypi private repository gcs google cloud storage",
+    url="https://github.com/ethronsoft/gcspypi",
+    include_package_data=True,
+    description="Private packages repository backed by Google Cloud Storage",
+    packages=['ethronsoft','ethronsoft.gcspypi'],
+    install_requires=[
+        "google-cloud-storage==1.5.0"
+    ],
+    entry_points={
+        'console_scripts': [
+            'gcspypi = ethronsoft.gcspypi.__main__:main'
+        ]
+    }
 )
