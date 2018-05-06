2a06c80e1127ada7ec73fd5693e1c2020b2ca62f
Edd <ef@ethronsoft.com>
2018-05-06 11:49:54 +0100

    modified relative imports to work for py2.7 and 3.6

--- test/package_builder_test.py
+++ test/package_builder_test.py
@@ -1,14 +1,14 @@
 import os
 import sys
 import unittest
-sys.path.append(os.path.join(os.path.dirname(__file__),"../ethronsoft/gcspypi"))
-from pb import *
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from ethronsoft.gcspypi import pb
 
 class PackageBuilderTest(unittest.TestCase):
 
     def test_src(self):
         dir = os.path.dirname(os.path.abspath(__file__))
-        pkg = PackageBuilder(os.path.join(dir,"data","dist","test_package-1.0.0.tar.gz")).build()
+        pkg = pb.PackageBuilder(os.path.join(dir,"data","dist","test_package-1.0.0.tar.gz")).build()
         self.assertEqual(pkg.name, "test-package")
         self.assertEqual(pkg.version, "1.0.0")
         self.assertEqual(pkg.requirements, set(["test-dep1", "test-dep2"]))
@@ -16,7 +16,7 @@ class PackageBuilderTest(unittest.TestCase):
 
     def test_wheel(self):
         dir = os.path.dirname(os.path.abspath(__file__))
-        pkg = PackageBuilder(os.path.join(dir,"data","dist","test_package-1.0.0-py2-none-any.whl")).build()
+        pkg = pb.PackageBuilder(os.path.join(dir,"data","dist","test_package-1.0.0-py2-none-any.whl")).build()
         self.assertEqual(pkg.name, "test-package")
         self.assertEqual(pkg.version, "1.0.0")
         self.assertEqual(pkg.requirements, set(["test-dep1", "test-dep2"]))
--- test/utils_test.py
+++ test/utils_test.py
@@ -2,20 +2,20 @@ import sys
 import os
 import unittest
 
-sys.path.append(os.path.join(os.path.dirname(__file__),"../ethronsoft/gcspypi"))
-from pb import Package
-from utils import *
-import pm
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from ethronsoft.gcspypi import pb 
+from ethronsoft.gcspypi import utils 
+from ethronsoft.gcspypi import pm 
 
 class TestUtils(unittest.TestCase):
 
     def setUp(self):
         self.data = [
-            Package("hello1", "0.0.1"),
-            Package("hello1", "0.0.2"),
-            Package("hello1", "0.1.2"),
-            Package("hello2", "0.1.0"),
-            Package("hello2", "0.1.1"),
+            pb.Package("hello1", "0.0.1"),
+            pb.Package("hello1", "0.0.2"),
+            pb.Package("hello1", "0.1.2"),
+            pb.Package("hello2", "0.1.0"),
+            pb.Package("hello2", "0.1.1"),
         ]
 
     def test_version_cmp(self):
@@ -23,72 +23,72 @@ class TestUtils(unittest.TestCase):
         for syntax in ["toolkit>=1.0.3"]:
             pkg = pkg_mgr.search(syntax)
             print(pkg)
-        self.assertLess(pkg_comp_version("1.05.0", "1.5.0"), 0)
-        self.assertGreater(pkg_comp_version("1.0.15", "1.0.2"), 0)
-        self.assertGreater(pkg_comp_version("10.0.0", "9.999.999"), 0)
+        self.assertLess(utils.pkg_comp_version("1.05.0", "1.5.0"), 0)
+        self.assertGreater(utils.pkg_comp_version("1.0.15", "1.0.2"), 0)
+        self.assertGreater(utils.pkg_comp_version("10.0.0", "9.999.999"), 0)
 
     def test_cmp_bisect(self):
         for i in range(len(self.data)):
-            self.assertEqual(i, cmp_bisect(self.data, self.data[i], pkg_comp_name_version))
-        self.assertEqual(0, cmp_bisect(self.data, Package("hello1", "0.0.0"), pkg_comp_name_version))
-        self.assertEqual(2, cmp_bisect(self.data, Package("hello1", "0.0.3"), pkg_comp_name_version))
-        self.assertEqual(len(self.data), cmp_bisect(self.data, Package("hello3", "0.0.0"), pkg_comp_name_version))
+            self.assertEqual(i, utils.cmp_bisect(self.data, self.data[i], utils.pkg_comp_name_version))
+        self.assertEqual(0, utils.cmp_bisect(self.data, pb.Package("hello1", "0.0.0"), utils.pkg_comp_name_version))
+        self.assertEqual(2, utils.cmp_bisect(self.data, pb.Package("hello1", "0.0.3"), utils.pkg_comp_name_version))
+        self.assertEqual(len(self.data), utils.cmp_bisect(self.data, pb.Package("hello3", "0.0.0"), utils.pkg_comp_name_version))
 
     def test_lower(self):
-        self.assertEqual(Package("hello1", "0.0.2"),
-                         lower(self.data, Package("hello1", "0.0.15"), pkg_comp_name_version))
+        self.assertEqual(pb.Package("hello1", "0.0.2"),
+                         utils.lower(self.data, pb.Package("hello1", "0.0.15"), utils.pkg_comp_name_version))
         self.assertEqual(None,
-                         lower(self.data, Package("hello1", "0.0.1"), pkg_comp_name_version))
-        self.assertEqual(Package("hello2", "0.1.1"),
-                         lower(self.data, Package("hello3", "0.0.1"), pkg_comp_name_version))
+                         utils.lower(self.data, pb.Package("hello1", "0.0.1"), utils.pkg_comp_name_version))
+        self.assertEqual(pb.Package("hello2", "0.1.1"),
+                         utils.lower(self.data, pb.Package("hello3", "0.0.1"), utils.pkg_comp_name_version))
 
     def test_floor(self):
-        self.assertEqual(Package("hello1", "0.0.1"),
-                         floor(self.data, Package("hello1", "0.0.1"), pkg_comp_name_version))
+        self.assertEqual(pb.Package("hello1", "0.0.1"),
+                         utils.floor(self.data, pb.Package("hello1", "0.0.1"), utils.pkg_comp_name_version))
         self.assertEqual(None,
-                         floor(self.data, Package("hello1", "0.0.05"), pkg_comp_name_version))
-        self.assertEqual(Package("hello1", "0.0.2"),
-                         lower(self.data, Package("hello1", "0.0.3"), pkg_comp_name_version))
+                         utils.floor(self.data, pb.Package("hello1", "0.0.05"), utils.pkg_comp_name_version))
+        self.assertEqual(pb.Package("hello1", "0.0.2"),
+                         utils.lower(self.data, pb.Package("hello1", "0.0.3"), utils.pkg_comp_name_version))
 
     def test_higher(self):
-        self.assertEqual(Package("hello1", "0.0.1"),
-                         higher(self.data, Package("hello1", "0.0.0"), pkg_comp_name_version))
-        self.assertEqual(Package("hello1", "0.1.2"),
-                         higher(self.data, Package("hello1", "0.0.3"), pkg_comp_name_version))
+        self.assertEqual(pb.Package("hello1", "0.0.1"),
+                         utils.higher(self.data, pb.Package("hello1", "0.0.0"), utils.pkg_comp_name_version))
+        self.assertEqual(pb.Package("hello1", "0.1.2"),
+                         utils.higher(self.data, pb.Package("hello1", "0.0.3"), utils.pkg_comp_name_version))
         self.assertEqual(None,
-                         higher(self.data, Package("hello2", "0.1.1"), pkg_comp_name_version))
+                         utils.higher(self.data, pb.Package("hello2", "0.1.1"), utils.pkg_comp_name_version))
 
     def test_ceiling(self):
-        self.assertEqual(Package("hello1", "0.0.1"),
-                         ceiling(self.data, Package("hello1", "0.0.0"), pkg_comp_name_version))
-        self.assertEqual(Package("hello1", "0.0.1"),
-                         ceiling(self.data, Package("hello1", "0.0.1"), pkg_comp_name_version))
-        self.assertEqual(Package("hello1", "0.1.2"),
-                         ceiling(self.data, Package("hello1", "0.0.3"), pkg_comp_name_version))
+        self.assertEqual(pb.Package("hello1", "0.0.1"),
+                         utils.ceiling(self.data, pb.Package("hello1", "0.0.0"), utils.pkg_comp_name_version))
+        self.assertEqual(pb.Package("hello1", "0.0.1"),
+                         utils.ceiling(self.data, pb.Package("hello1", "0.0.1"), utils.pkg_comp_name_version))
+        self.assertEqual(pb.Package("hello1", "0.1.2"),
+                         utils.ceiling(self.data, pb.Package("hello1", "0.0.3"), utils.pkg_comp_name_version))
         self.assertEqual(None,
-                         ceiling(self.data, Package("hello3", "0.0.3"), pkg_comp_name_version))
+                         utils.ceiling(self.data, pb.Package("hello3", "0.0.3"), utils.pkg_comp_name_version))
 
     def test_range_query(self):
-        self.assertEqual(Package("hello1", "0.0.2"),
-                         pkg_range_query(self.data, "hello1", ">", "0.0.1"))
-        self.assertEqual(Package("hello1", "0.0.2"),
-                         pkg_range_query(self.data, "hello1", ">", "0.0.1", "<=","0.0.2"))
+        self.assertEqual(pb.Package("hello1", "0.0.2"),
+                         utils.pkg_range_query(self.data, "hello1", ">", "0.0.1"))
+        self.assertEqual(pb.Package("hello1", "0.0.2"),
+                         utils.pkg_range_query(self.data, "hello1", ">", "0.0.1", "<=","0.0.2"))
         self.assertEqual(None,
-                         pkg_range_query(self.data, "hello1", ">", "0.0.1", "<","0.0.2"))
-        self.assertEqual(Package("hello1", "0.0.1"),
-                         pkg_range_query(self.data, "hello1", ">=", "0.0.1", "<","0.0.2"))
+                         utils.pkg_range_query(self.data, "hello1", ">", "0.0.1", "<","0.0.2"))
+        self.assertEqual(pb.Package("hello1", "0.0.1"),
+                         utils.pkg_range_query(self.data, "hello1", ">=", "0.0.1", "<","0.0.2"))
         self.assertEqual(None,
-                         pkg_range_query(self.data, "hello1", ">", "0.0.1", "<", "0.0.1"))
-        self.assertEqual(Package("hello1", "0.0.2"),
-                         pkg_range_query(self.data, "hello1", "==", "0.0.2"))
-        self.assertEqual(Package("hello1", "0.0.1"),
-                         pkg_range_query(self.data, "hello1", "<", "0.0.2"))
+                         utils.pkg_range_query(self.data, "hello1", ">", "0.0.1", "<", "0.0.1"))
+        self.assertEqual(pb.Package("hello1", "0.0.2"),
+                         utils.pkg_range_query(self.data, "hello1", "==", "0.0.2"))
+        self.assertEqual(pb.Package("hello1", "0.0.1"),
+                         utils.pkg_range_query(self.data, "hello1", "<", "0.0.2"))
         self.assertEqual(None,
-                         pkg_range_query(self.data, "hello1", ">", "1.1.2"))
-        self.assertEqual(Package("hello1", "0.1.2"),
-                         pkg_range_query(self.data, "hello1"))
-        self.assertEqual(Package("hello1", "0.0.2"),
-                         pkg_range_query(self.data, "hello1", "<"))
+                         utils.pkg_range_query(self.data, "hello1", ">", "1.1.2"))
+        self.assertEqual(pb.Package("hello1", "0.1.2"),
+                         utils.pkg_range_query(self.data, "hello1"))
+        self.assertEqual(pb.Package("hello1", "0.0.2"),
+                         utils.pkg_range_query(self.data, "hello1", "<"))
 
 
 if __name__ == '__main__':
